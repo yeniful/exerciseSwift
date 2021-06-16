8679dffaf3ea97a6535556f734e16e2e349b8a05
yeniful <yeniful@gmail.com>
2021-06-17 02:55:35 +0900

    Methods for Responding to View-Related Events

---
+++ ViewTransition/ViewTransition/SecondViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SecondViewController.swift
+//  ViewTransition
+//
+//  Created by Yeni Hwang on 2021/06/16.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("SecondViewController의 view가 메모리에 로드 됨")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("SecondViewController의 view가 화면에 보여질 예정")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("SecondViewController의 view가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("SecondViewController의 view가 화면에서 사라질 예정")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("SecondViewController의 view가 화면에서 사라짐")
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("SecondViewController의 view가 subview를 레이아웃 하려 함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("SecondViewController의 view가 subview를 레이아웃 함")
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func popToprev() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func dismissModal() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
